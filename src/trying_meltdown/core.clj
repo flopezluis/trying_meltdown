c13ab0c5b24502059cecac221eaf431c5e7464fa
Felix Lopez <hola@flopezluis.es>
2014-10-08 17:24:19 +0200

    refactor to use reactor and events

--- src/trying_meltdown/core.clj
+++ src/trying_meltdown/core.clj
@@ -5,39 +5,40 @@
             [clojurewerkz.meltdown.selectors :as ms :refer [$ R]]
             [clojurewerkz.meltdown.streams :as mstr]))
 
-;;Counters to tet how many contacts were sent and received
+;;Counters to know how many contacts were sent and received
 (def counter (agent 0))
 (def received (agent 0))
 
+(def reactor (mr/create :dispatcher-type :ring-buffer))
 (def channel (mstr/create))
 
-(defn publish [data]
-  "send data to the channel"
-  (send counter inc)
-  (mstr/accept channel data)
-  (println "Sent " @counter))
-
 (defn consume []
-  "consumer messages in batches of 10 and wait 300 ms"
-  (let [one-batch (mstr/batch* 10 channel)]
-      (mstr/consume one-batch (fn [i]
-                                (send received inc)
-                                ( Thread/sleep 300)
-                                ))))
+  "consumer messages, wait 300 ms"
+  (mr/on reactor ($ "key") (fn [event]
+     (send received inc)
+    (Thread/sleep 300)
+    (println "received" @received)
+  ))
+)
+
+(defn publish [vcard]
+  (send counter inc)
+  (mr/notify reactor "key" vcard)
+  (println "sent" @counter)
+)
 
 (defn send_batch []
-  (dorun (map publish (map createVcard (take  1000 (iterate inc 1)))))
+  (dorun (map publish (map createVcard (take 10000 (iterate inc 1)))))
 )
 
 (defn -main
   [& args]
-  (def reactor (mr/create :dispatcher-type :ring-buffer))
   (.start (Thread. consume))
   (.start (Thread. send_batch))
   (.start (Thread. send_batch))
   (println (read-line))
   (println "Sent " @counter)
-  (println "Received" (* 10 @received))
+  (println "Received" @received)
   )
 
 
